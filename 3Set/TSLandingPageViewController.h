635455833148c74db2c8c4e0b6fdd2091693ff26
Julius Elinson <jelinson@hmc.edu>
2013-06-14 17:38:04 +0200

    got landing page transition

--- 3Set/TSLandingPageViewController.h
+++ 3Set/TSLandingPageViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TSLandingPageNavigationController : UINavigationController
+@interface TSLandingPageViewController : UIViewController
 
 @end
---
+++ 3Set/TSLandingPageViewController.m
@@ -0,0 +1,43 @@
+//
+//  TSViewController.m
+//  3Set
+//
+//  Created by Julius Elinson on 2013.06.12..
+//  Copyright (c) 2013 AIT. All rights reserved.
+//
+
+#import "TSLandingPageViewController.h"
+
+@interface TSLandingPageViewController ()
+
+@end
+
+@implementation TSLandingPageViewController
+
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:YES animated:animated];
+    [super viewWillAppear:animated];
+    NSLog(@"viewWillAppear called");
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [self.navigationController setNavigationBarHidden:NO animated:animated];
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
