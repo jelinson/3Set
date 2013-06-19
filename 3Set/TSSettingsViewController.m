7ca13786326e5af2fe65d406212b1f9a269f3af9
Julius Elinson <jelinson@hmc.edu>
2013-06-19 03:01:36 +0200

    implemented hide and ncards settings

--- 3Set/TSSettingsViewController.m
+++ 3Set/TSSettingsViewController.m
@@ -7,12 +7,15 @@
 //
 
 #import "TSSettingsViewController.h"
+#import "TSSettingManager.h"
 
 @interface TSSettingsViewController ()
 
 @end
 
 @implementation TSSettingsViewController
+
+@synthesize numberOfCardsSelector, backgroundColorLabel, hideFrameSwitch;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -23,6 +26,53 @@
     return self;
 }
 
+-(void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    TSSettingManager* sm = [TSSettingManager getSettingManagerInstance];
+    
+    int numberOfCards = [sm numberOfCards];
+    switch (numberOfCards) {
+        case 9:
+            [numberOfCardsSelector setSelectedSegmentIndex:0];
+            break;
+        case 12:
+            [numberOfCardsSelector setSelectedSegmentIndex:1];
+            break;
+        case 15:
+            [numberOfCardsSelector setSelectedSegmentIndex:2];
+            break;
+        default:
+            NSLog(@"WARNING: Corrupted setting");
+    }
+    
+    bool hideFrame = [sm hideFrameDuringPlay];
+    [hideFrameSwitch setOn:hideFrame];
+}
+
+- (IBAction)numberOfCardsValueChanged:(id)sender {
+    int index = [numberOfCardsSelector selectedSegmentIndex];
+    int numberOfCards;
+    
+    switch (index) {
+        case 0:
+            numberOfCards = 9;
+            break;
+        case 1:
+            numberOfCards = 12;
+            break;
+        case 2:
+            numberOfCards = 15;
+            break;
+    }
+    
+    [[TSSettingManager getSettingManagerInstance] updateNumberOfCards:numberOfCards];
+}
+
+- (IBAction)hideFrameValueChanged:(id)sender {
+    BOOL hideFrame = [hideFrameSwitch isOn];
+    [[TSSettingManager getSettingManagerInstance] updateHideFrameDuringPlay:hideFrame];
+}
 
 
 #pragma mark - Table view delegate
